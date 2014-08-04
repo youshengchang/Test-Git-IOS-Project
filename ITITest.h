7120f0427fee997dd5d80f592a95b5441dff1eff
youshengchang <changyousheng@yahoo.com>
2014-08-03 21:14:30 -0700

    add a test class

---
+++ ITITest.h
@@ -0,0 +1,13 @@
+//
+//  ITITest.h
+//  Git Test
+//
+//  Created by yousheng chang on 8/3/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITITest : XYZPerson
+
+@end
---
+++ ITITest.m
@@ -0,0 +1,13 @@
+//
+//  ITITest.m
+//  Git Test
+//
+//  Created by yousheng chang on 8/3/14.
+//  Copyright (c) 2014 InfoTech Inc. All rights reserved.
+//
+
+#import "ITITest.h"
+
+@implementation ITITest
+
+@end
